2a6b5099d0273f65d4856b29b89143accefc518f
rickyxyz <56119701+rickyxyz@users.noreply.github.com>
2023-11-05 00:43:41 +0700

    test deploy action

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
 
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig(({ mode }) => {
+  const env = {
+    VITE_BASE_URL: process.env.VITE_BASE_URL,
+    ...loadEnv(mode, process.cwd(), 'VITE_'),
+  };
   return {
     plugins: [react()],
     test: {
@@ -12,6 +16,6 @@ export default defineConfig(({ mode }) => {
       environment: 'jsdom',
       setupFiles: ['setupTests.ts'],
     },
-    base: mode === 'production' ? '/vite-deploy-test/' : '/',
+    base: mode === 'production' ? env.VITE_BASE_URL : '/',
   };
 });
