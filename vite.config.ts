7e27168336f6ac68830bea1e62a909637ad678c8
rickyxyz <56119701+rickyxyz@users.noreply.github.com>
2023-11-05 01:53:21 +0700

    add storybook to deploy

--- vite.config.ts
+++ vite.config.ts
@@ -9,9 +9,6 @@ export default defineConfig(({ mode }) => {
     mode === 'production'
       ? { VITE_BASE_URL: process.env.VITE_BASE_URL }
       : { ...loadEnv(mode, process.cwd(), 'VITE_') };
-  console.log('🔥');
-  console.log(process.env);
-  console.log(mode, env.VITE_BASE_URL);
   return {
     plugins: [react()],
     test: {
