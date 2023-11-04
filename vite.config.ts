f30f64dce8b84bcb2396c5f9158c3cd4a5ed2b18
rickyxyz <56119701+rickyxyz@users.noreply.github.com>
2023-11-04 21:48:50 +0700

    add build and test to deploy action

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: ['setupTests.ts'],
-  },
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      setupFiles: ['setupTests.ts'],
+    },
+    base: mode === 'production' ? '/vite-deploy-test/' : '/',
+  };
 });
